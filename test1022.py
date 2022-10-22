1fe2e09cdecacb3ed1ccb86afc4938fd4f230a85
Luc <2235615010@qq.com>
2022-10-22 23:22:59 +0800

    object-oriented

---
+++ test1022.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self):
+        self.weight = None
+
+    wheels = 4
+
+
+def drive(self):
+    print('drive')
+
+
+car = Car()
+var = car.wheels
+var.drive()
+car.wheels = 3
+print(car.wheels)
+car.color = "res"
+Car.weight = "1t"
+print(car.color)
+print(car.weight)
+
+
